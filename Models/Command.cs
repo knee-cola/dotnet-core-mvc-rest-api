cae4f3e4c77dcbede9c2f8b2c6d79981c7d547f3
Nikola Derežić <nikola.derezic@ngit.hr>
2020-10-07 14:09:19 +0200

    kreiran model

---
+++ Models/Command.cs
@@ -0,0 +1,9 @@
+namespace dotnet_core_mvc_rest_api.Models {
+    public class Command
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+        public string Platform { get; set; }
+    }
+}
