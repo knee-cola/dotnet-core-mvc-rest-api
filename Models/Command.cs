11d7652fad434fabf98969b1b15470ec41b042fd
Nikola Derežić <nikola.derezic@ngit.hr>
2020-10-09 09:13:13 +0200

    property annotations added to model

--- Models/Command.cs
+++ Models/Command.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace dotnet_core_mvc_rest_api.Models {
     public class Command
     {
+        // we don't really need to place `[Key]` since `Id` is `[Key]` and `[Required]` by default (automatically)
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
         public string HowTo { get; set; }
+        [Required]
         public string Line { get; set; }
+        
+        [Required]
         public string Platform { get; set; }
     }
 }
